bf52d452e0da999873c5c86fe1bd3488eb38dee3
FL1NEE <70909941+FL1NEE@users.noreply.github.com>
2023-09-25 21:54:45 +0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
-
+"""
+:authors: FL1NEE
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2023 FL1NEE
+"""
 from .pycrystalpay import *
+
+__author__ = "FL1NEE"
+__version__ = "1.0.0"
+__email__ = "daniilsergeev786@gmail.com"
